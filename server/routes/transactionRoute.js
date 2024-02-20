ca684a4c835a37e19816078574ffdb5b485b33ef
Sujal Tangde <sujaltagade01@gmail.com>
2024-02-20 09:45:44 +0530

    backend done

--- server/routes/transactionRoute.js
+++ server/routes/transactionRoute.js
@@ -1,4 +1,4 @@
-const { initializeTransaction, transactions } = require("../controllers/transactionControls");
+const { initializeTransaction, transactions, statistics, barChartData, pieChartData, analytics } = require("../controllers/transactionControls");
 
 const express = require("express");
 
@@ -7,6 +7,14 @@ const router = express.Router();
 router.route("/initialize_table").get(initializeTransaction);
 
 router.route('/transactions').get(transactions);
+
+router.route('/statistics/:month').get(statistics);
+
+router.route('/barChartData/:month').get(barChartData);
+
+router.route('/pieChartData/:month').get(pieChartData);
+
+router.route('/analytics/:month').get(analytics);
 
 
 module.exports = router;
